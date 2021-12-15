1e94efb89e8bc4ad00bb50d6b72f325ad7a02219
Alex Stuart <77671874+noob-af-bd@users.noreply.github.com>
2021-12-15 12:47:31 +0600

    Update pyrogramEngine.py

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -143,9 +143,7 @@ class TgUploader:
         except RPCError as e:
             LOGGER.error(f"RPCError: {e} {up_path}")
         except Exception as err:
-            LOGGER.info(str(err))
-            self.is_cancelled = True
-            self.__listener.onUploadError(str(err))
+            LOGGER.error(str(err) + str(up_path))
         if self.thumb is None and thumb is not None and os.path.lexists(thumb):
             os.remove(thumb)
         if not self.is_cancelled:
