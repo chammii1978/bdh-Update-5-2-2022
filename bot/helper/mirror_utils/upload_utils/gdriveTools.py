9753e176753223fc3186385e30341b4b788aedfa
Alex Stuart <77671874+noob-af-bd@users.noreply.github.com>
2021-12-15 12:18:24 +0600

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -740,7 +740,7 @@ class GoogleDriveHelper:
         for content in self.telegraph_content:
             self.path.append(
                 telegraph.create_page(
-                    title='Mirror-Leech-Bot Drive Search',
+                    title='Bangladesh Hoarding',
                     content=content
                 )["path"]
             )
